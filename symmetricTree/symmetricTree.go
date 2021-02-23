4b05171c87e893dd2c39ccaae2a1d5d3fb56276b
xuxiaochen <xixiaochen@conew.com>
2021-02-23 11:50:30 +0800

    搞一波对称二叉树

--- symmetricTree/symmetricTree.go
+++ symmetricTree/symmetricTree.go
@@ -1,24 +1,28 @@
 package main
 
-import "golang-algorithm/binaryTree"
-
+import (
+	"golang-algorithm/binaryTree"
+)
+//是否是对称二叉树
 func main() {
 
 }
 
 func isSymmetricTree(node *binaryTree.TreeNode) bool {
-	if node == nil {
+	if node == nil{
 		return true
 	}
-	return isSame(node.Left, node.Right)
+	aa := isSame(node.Left,node.Right)
+	return aa
 }
 
 func isSame(l, r *binaryTree.TreeNode) bool {
 	if l == nil && r == nil {
 		return true
 	}
+
 	if l == nil || r == nil{
 		return false
 	}
-	return l.Val == r.Val && isSame(l.Left,r.Right) && isSame(r.Right,l.Left)
+	return l.Val == r.Val && isSame(l.Left,r.Right) && isSame(l.Right,r.Left)
 }
